ac09fee39298d76258723b26664c497572dbe787
OrAbitbul <117725382+OrAbitbul@users.noreply.github.com>
2023-02-11 16:49:32 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! v1'
+    return 'Hello, World! v2'
 
 if __name__ == "__main__":
    port = int(os.environ.get("PORT", 5000))
